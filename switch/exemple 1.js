3ce8d2d859503328aaf40218c3d807dac24f69bc
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-05 08:57:27 +0100

    Add files via upload

---
+++ switch/exemple 1.js
@@ -0,0 +1,18 @@
+let consommable = "chocolat";
+
+switch(consommable) {
+
+
+    case "carotte" :
+    case "pomme de terre":
+    case "courgette":
+        console.log('ceci est un légume');
+        break;
+
+        case "banane":
+        console.log('ceci est un fruit');
+        break;
+
+ default: 
+ console.log("ceci n'est ni un fruit ni un légume");
+}
