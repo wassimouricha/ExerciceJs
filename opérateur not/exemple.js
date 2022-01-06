b135521ab9b7c71b3af2bee9066e5f9c87a82925
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-06 08:25:30 +0100

    Add files via upload

---
+++ opérateur not/exemple.js
@@ -0,0 +1,8 @@
+let x = 7;
+//! permet d'inverser l'information donc ici on aura l'inverse de x<5
+if( !x <5) {
+    console.log('x est inférieur à 5.');
+} else{
+    console.log("x n'est pas inférieur à 5.");
+}
+
