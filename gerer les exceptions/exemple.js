eeda1bb950eb99301d827b74401ed332d1e40e0f
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-06 16:49:48 +0100

    Update exemple.js

--- gerer les exceptions/exemple.js
+++ gerer les exceptions/exemple.js
@@ -1,10 +1,31 @@
-try {
-//il va chercher une erreur
-alert(hello);
+
+    try {
+        let recompense = prompt("choisissez une récompense : épée, arc, haches ")
+        
+        switch(recompense){
+    
+            case 'épée' :
+                degats = 40;
+                break;
+            case 'arc' :
+                degats = 30;
+                break;
+            case 'haches' :
+                degats = 20;
+                break;
+                default: 
+                    throw new Error('vous ne pouvez pas tricher. ');
+        }
+    
+    alert('vous avez choisi : ' + recompense + '(' + degats + 'dégats).');
+    
+    
 } 
 
 catch(error){
-// si il y a une erreur le block catch va s'executer 
-alert("la variable hello n'existe pas ");
-// console.error(error.stack)
+    //erreur !
+alert(error)
+}
+finally{
+    alert('fin du programme');
 }
