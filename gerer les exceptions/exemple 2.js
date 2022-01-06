7b0239182e26dcf0ba5538efcb3b37720b35b3b4
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-06 16:48:06 +0100

    Add files via upload

---
+++ gerer les exceptions/exemple 2.js
@@ -0,0 +1,28 @@
+
+    try {
+        let recompense = prompt("choisissez une récompense : épée, arc, haches ")
+        
+        switch(recompense){
+    
+            case 'épée' :
+                degats = 40;
+                break;
+            case 'arc' :
+                degats = 30;
+                break;
+            case 'haches' :
+                degats = 20;
+                break;
+                default: 
+                    throw new Error('vous ne pouvez pas tricher. ');
+        }
+    
+    alert('vous avez choisi : ' + recompense + '(' + degats + 'dégats).');
+    
+    
+} 
+
+catch(error){
+    //erreur !
+alert(error)
+}
