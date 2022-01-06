d88ea6f2950dfc1e9759a95269f26f8168642a93
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-06 16:30:46 +0100

    Add files via upload

---
+++ break et continue/exemple.js
@@ -0,0 +1,15 @@
+let i = 0 ;
+
+while (i < 5) {
+
+    if( i == 3) {
+        // i++
+        // continue;
+        //continue permet de passer à la prochaine itération et donc la boucle va recommencer 
+        break;
+    } //break permet d'arreter la boucle 
+
+    console.log('ligne : ' + i);
+    i++;
+  
+}
