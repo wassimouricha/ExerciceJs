e12badd428eecae3fa133922836491d54add5ec9
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-07 14:55:15 +0100

    Add files via upload

---
+++ portée des variables/app.js
@@ -0,0 +1,16 @@
+let letus = "globale"
+var varus = "globale"
+
+if(true){
+
+    let letus = "locale"
+    var varus = "locale"
+
+    console.log("lrt : " + letus);
+    console.log("var : " + varus);
+
+}
+
+
+console.log("lrt : " + letus);
+console.log("var : " + varus);
