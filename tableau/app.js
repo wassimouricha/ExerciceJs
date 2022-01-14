d95e514551aa509ddaa509225f218d155a816598
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-14 11:45:21 +0100

    Update app.js

--- tableau/app.js
+++ tableau/app.js
@@ -1,3 +1,17 @@
 let monTableau = new Array ('un' , 'deux' , 'trois');
 let monTableau =  Array ('un' , 'deux' , 'trois');
 let monTableau = [ 'un' , 'deux ' , ' trois'];
+/*ceci est l'ancienne façon de rediger un tableau à plusieur dimension */
+let montableau3d = (
+    Array('mark' , 'jeff' ,'bill'),
+    Array('zuckerberg','bezos','gates' )
+);
+
+/* un tableau avec ub autre tableau à l'interieur est un tableau a plusieurs dimension */
+
+let monTableau2d = [
+
+[ 'mark','jeff' , 'bill'],
+['zuckerberg','bezos','gates']
+
+];
