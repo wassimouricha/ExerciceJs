ffeb0873509c5ba28a5ae1d654819d91045dcbbb
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2022-01-20 09:01:36 +0100

    Add files via upload

--- tableau/app.js
+++ tableau/app.js
@@ -1,6 +1,13 @@
 let monTableau = new Array ('un' , 'deux' , 'trois');
 let monTableau =  Array ('un' , 'deux' , 'trois');
 let monTableau = [ 'un' , 'deux ' , ' trois'];
+//push permet d'ajouter un élément a la fin mon tableau 
+//unshift permet d'ajouter un élément au debut de mon tableau
+monTableau.push('cinq');
+monTableau.unshift('zero');
+
+console.log( monTableau );
+
 /*ceci est l'ancienne façon de rediger un tableau à plusieur dimension */
 let montableau3d = (
     Array('mark' , 'jeff' ,'bill'),
@@ -15,3 +22,10 @@ let monTableau2d = [
 ['zuckerberg','bezos','gates']
 
 ];
+
+let monTableauAssociatif = {
+'prenom' : 'Mark',
+'nom' : 'Zuckerberg',
+'poste' : 'PDG de Facebook'
+
+};
